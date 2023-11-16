7c24ed1ee40a0b01d02f7b8206066a9c3661c16e
Kauê Matheus <kauematheus7@hotmail.com>
2023-11-16 14:45:35 -0300

    logica de login e conversa
    Variaveis de login  + funções

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,45 @@
-//login ele
+//LOGIN ELEMENTS 
+const login = document.querySelector(".login");
+const loginForm = document.querySelector(".login__form");
+const loginInput = document.querySelector(".login__input");
+
+
+//CHAT ELEMENTS 
+const chat = document.querySelector(".chat");
+const chatForm = document.querySelector(".chat__form");
+const chatInput = document.querySelector(".chat__input");
+
+
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+]
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+
+
+const user = { id: "", name: "", color: "" };
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    console.log(user)
+};
+
+loginForm.addEventListener("submit", handleSubmit);
